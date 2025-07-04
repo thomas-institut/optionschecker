af1e725689b5c20461331e85b857d8937a477700
Rafael Nájera <rafael@najera.ca>
2025-07-04 12:18:24 +0200

    Yet another TS fix

--- OptionsChecker.d.ts
+++ OptionsChecker.d.ts
@@ -2,7 +2,7 @@
 
 export class OptionsChecker {
 
-    constructor(constructorOptions: any, contextStr: string|null, verbose? : boolean);
+    constructor(constructorOptions: any, contextStr?: string|null, verbose? : boolean);
     setDebug(debug: boolean): void;
     getCleanOptions(optionsObject: any): any;
     getDefaults(): any;
